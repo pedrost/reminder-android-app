397dba420df348ea82f9c9f401e4d54224fbf595
Pedro Sturmer <pee.dro_fs@hotmail.com>
2020-06-28 19:04:08 -0400

    Add encryption

--- app/src/main/java/com/example/todoapp/data/LoginDataSource.java
+++ app/src/main/java/com/example/todoapp/data/LoginDataSource.java
@@ -1,10 +1,25 @@
 package com.example.todoapp.data;
 
+import android.util.Log;
+
 import com.example.todoapp.data.model.CurrentUser;
 import com.example.todoapp.data.model.LoggedInUser;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.util.concurrent.ExecutionException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -12,9 +27,39 @@ import java.util.concurrent.ExecutionException;
 public class LoginDataSource {
 
     private DBHelper dbHelper;
+    private SecretKeySpec secret;
+    private LoginDataSource EncUtil;
 
     public void setDBHelper(DBHelper helper) {
         this.dbHelper = helper;
+    }
+
+    public static SecretKey generateKey(String password)
+            throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        return new SecretKeySpec(password.getBytes(), "AES");
+    }
+
+    public static byte[] encryptMsg(String message, SecretKey secret)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException
+    {
+        /* Encrypt the message. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        return cipherText;
+    }
+
+    public static String decryptMsg(byte[] cipherText, SecretKey secret)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException
+    {
+        /* Decrypt the message, given derived encContentValues and initialization vector. */
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        String decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return decryptString;
     }
 
     public Result<LoggedInUser> login(String email, String password) {
@@ -23,6 +68,7 @@ public class LoginDataSource {
             LoggedInUser user = dbHelper.findUserByEmail(email);
 
             // If not find any user, create one
+            Log.println(Log.ERROR, "finded user password", user.getPassword().toString());
             if(user.getPassword().equals("")) {
                 LoggedInUser newUser =
                         new LoggedInUser(
@@ -47,6 +93,7 @@ public class LoginDataSource {
             }
         } catch (Exception e) {
             Result result = new Result.Error(new IOException("Erro ao logar", e));
+            Log.println(Log.ERROR, "error ", e.toString());
             result.setErrorMessage("Erro ao logar");
             return result;
         }
